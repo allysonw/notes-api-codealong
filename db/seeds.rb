8fd9b9e970f3ac02735b37b8450b650cecc05b7f
Allyson Wesman <allysonwesman@gmail.com>
2018-05-19 14:50:23 -0700

    add js frontend folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Note.create([
+   {body: "This is a new cool note"},
+   {body: "But also the leap into electronic typesetting, remaining essentially unchanged"},
+   {body: "assages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},
+   {body: "ajority have suffered alteration in some form, by injected humour, o"},
+   {body: "Phasellus pellentesque magna sed ante sodales dapibus."},
+   {body: "Aliquam porta venenatis mattis. Vivamus rhoncus mauris ac luctus imperdiet. Pellentesque et arcu ut lorem semper laoreet. Praesent aliquam mauris sed sagittis auctor."}
+])
